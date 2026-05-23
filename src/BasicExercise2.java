5b88d14a113791ac235ca682db7553c5bc209e3b
mhong1208 <myhong11a32004@gmail.com>
2026-05-23 14:28:55 +0700

    del trash

--- src/BasicExercise2.java
+++
@@ -1,9 +0,0 @@
-public class BasicExercise2 {
-    public static void run() {
-        System.out.println("[Exercise 2] Check even/odd numbers from 1 to 5");
-        for (int i = 1; i <= 5; i++) {
-            String kind = (i % 2 == 0) ? "even" : "odd";
-            System.out.println(i + " is " + kind);
-        }
-    }
-}
